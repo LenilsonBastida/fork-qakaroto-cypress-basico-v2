df8d940980bf83f4ada0d0079886451d691879db
lenilson2 <lenilson@metryx.com.br>
2025-04-05 09:31:03 -0300

    feat: pequena alteração feita - Pós

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -10,7 +10,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   })
 
   it('preenche os campos obrigatórios e envia o formulário', function () {
-    cy.get('#firstName').type('Goku')
+    cy.get('#firstName').type('João')
     cy.get('#lastName').type('QA')
     cy.get('#email').type('qakarotto@gmail.com')
     cy.get('#open-text-area').type('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rhoncus rutrum malesuada. Nam a leo lacinia, facilisis diam quis, aliquam odio. Cras id orci eget leo mollis dapibus. Suspendisse egestas sem sit amet risus varius porttitor. Vestibulum eleifend, ligula in ultricies pretium, ipsum ante hendrerit lectus, at ultrices est quam ut massa. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis quis tincidunt augue. Vivamus id neque est. Proin at aliquam mi. Sed dui arcu, vehicula sit amet accumsan quis, tristique vitae lacus. Suspendisse consequat augue nec eros gravida elementum. Phasellus eget nisl nisl. Curabitur efficitur libero ac lectus tempus, in vehicula mi mattis. Suspendisse dignissim facilisis dolor, at lobortis felis laoreet non. Duis viverra ante ipsum, nec ullamcorper arcu mattis porttitor. Donec vulputate, massa sagittis accumsan gravida, tellus dolor maximus ligula, a hendrerit ex justo vel dui. Proin eget sapien neque. Mauris lobortis dui id venenatis porttitor. Vestibulum a diam varius, mollis nisl ac, placerat diam. Integer non arcu nec felis posuere tempus id in velit. Fusce gravida elit vitae nunc feugiat elementum. Curabitur varius quam ut odio tincidunt laoreet. Maecenas eget lectus gravida, rhoncus metus placerat, pulvinar enim. Etiam sit amet volutpat libero. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec sodales nunc quis malesuada varius. Curabitur eu euismod velit. Fusce iaculis consequat odio ac gravida. Nunc accumsan ultrices augue in vulputate. Sed fermentum vulputate metus, ut tincidunt quam posuere quis. Curabitur sollicitudin nunc leo, et accumsan ante dignissim quis. Maecenas quis sapien euismod, vulputate ipsum dictum, pharetra nibh. Nunc sed justo dolor. Phasellus a mauris vitae felis lobortis condimentum. Quisque laoreet efficitur lacus sit amet sollicitudin. Quisque eget justo erat. Suspendisse egestas dolor sit amet aliquet laoreet. Aenean porttitor mi at neque ultricies pellentesque. Fusce gravida consectetur lorem eget rutrum. Mauris dapibus aliquam ipsum at mattis. Suspendisse quis libero posuere, facilisis purus sit amet, fermentum turpis.', { delay: 0 })
@@ -19,7 +19,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   })
 
   it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function () {
-    cy.get('#firstName').type('Goku')
+    cy.get('#firstName').type('João')
     cy.get('#lastName').type('QA')
     cy.get('#email').type('wrong@gmail')
     cy.get('#open-text-area').type('Teste erro')
@@ -33,7 +33,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   })
 
   it('exibe mensagem de erro ao submeter o formulário sem telefone quando telefone é obrigatório', function () {
-    cy.get('#firstName').type('Goku')
+    cy.get('#firstName').type('João')
     cy.get('#lastName').type('QA')
     cy.get('#email').type('wrong@gmail')
     cy.get('#phone-checkbox').check()
@@ -43,7 +43,7 @@ describe('Central de Atendimento ao Cliente TAT', function () {
   })
 
   it('preenche e limpa campos nome, sobrenome, email e telefone', function () {
-    cy.get('#firstName').type('Goku').should('have.value', 'Goku').clear('').should('have.value', '')
+    cy.get('#firstName').type('João').should('have.value', 'João').clear('').should('have.value', '')
     cy.get('#lastName').type('QA').should('have.value', 'QA')
     cy.get('#lastName').clear('').should('have.value', '')
     cy.get('#email').type('qakarotto@gmail.com').should('have.value', 'qakarotto@gmail.com')
